ad818a213f5f2ef61caefe1994571574dd3eafc0
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-11-24 09:31:13 -0500

    style: set up tailwindcss auto-sort plugin for prettier

--- src/components/DisplayRecords.tsx
+++ src/components/DisplayRecords.tsx
@@ -1,11 +1,19 @@
-import { Exercise } from '../types';
+import { Exercise } from "../types";
 
 type DisplayRecordsProp = {
   exercises: Exercise[];
-  handleEditExercise: ({ currentTarget }: React.MouseEvent<HTMLButtonElement>) => void;
-  handleDeleteExercise: ({ currentTarget }: React.MouseEvent<HTMLButtonElement>) => void;
-  handleEditRecord: ({ currentTarget }: React.MouseEvent<HTMLButtonElement>) => void;
-  handleDeleteRecord: ({ currentTarget }: React.MouseEvent<HTMLButtonElement>) => void;
+  handleEditExercise: ({
+    currentTarget,
+  }: React.MouseEvent<HTMLButtonElement>) => void;
+  handleDeleteExercise: ({
+    currentTarget,
+  }: React.MouseEvent<HTMLButtonElement>) => void;
+  handleEditRecord: ({
+    currentTarget,
+  }: React.MouseEvent<HTMLButtonElement>) => void;
+  handleDeleteRecord: ({
+    currentTarget,
+  }: React.MouseEvent<HTMLButtonElement>) => void;
   setShowAddExerciseModal: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
@@ -27,58 +35,65 @@ export default function DisplayRecords({
   }
 
   return (
-    <div className='max-w-[640px] w-full m-auto'>
-      <h2 className='text-2xl font-bold'>Current Progress</h2>
-      <div className='flex-col flex w-full'>
+    <div className="m-auto w-full max-w-[640px]">
+      <h2 className="text-2xl font-bold">Current Progress</h2>
+      <div className="flex w-full flex-col">
         {exercises.map((exercise) => (
-          <div key={exercise.id} className='flex w-full flex-1 min-h-[80px] group'>
-            <div className='flex-col w-1/2 flex-1 justify-between flex bg-zinc-800 group-first:rounded-tl-md'>
-              <div className='flex-col p-2'>
-                <p className='text-md'>{exercise.name}</p>
-                <p className='text-xs'>{exercise.note}</p>
+          <div
+            key={exercise.id}
+            className="group flex min-h-[80px] w-full flex-1"
+          >
+            <div className="flex w-1/2 flex-1 flex-col justify-between bg-zinc-800 group-first:rounded-tl-md">
+              <div className="flex-col p-2">
+                <p className="text-md">{exercise.name}</p>
+                <p className="text-xs">{exercise.note}</p>
               </div>
-              <div className='flex justify-self-end gap-[1px]'>
+              <div className="flex gap-[1px] justify-self-end">
                 <button
                   id={exercise.id}
                   onClick={handleEditExercise}
-                  className='text-[10px] bg-zinc-700 hover:bg-white text-zinc-100 hover:text-zinc-900 py-2 px-2 w-full'>
+                  className="w-full bg-zinc-700 px-2 py-2 text-[10px] text-zinc-100 hover:bg-white hover:text-zinc-900"
+                >
                   Edit
                 </button>
                 <button
                   id={exercise.id}
                   onClick={handleDeleteExercise}
-                  className='text-[10px] bg-zinc-700 hover:bg-red-500 text-zinc-100 py-2 px-2  w-full'>
+                  className="w-full bg-zinc-700 px-2 py-2 text-[10px] text-zinc-100  hover:bg-red-500"
+                >
                   Delete
                 </button>
               </div>
             </div>
             {/* display last four records only */}
-            <div className='flex flex-1 w-1/2'>
+            <div className="flex w-1/2 flex-1">
               {exercise.records.slice(-1).map((record) => (
                 <div
                   key={record.date}
-                  className='flex-col bg-zinc-200 last:bg-white text-zinc-500 last:text-zinc-900 last:rounded-r-md text-center min-w-[100px] w-full relative flex justify-between shadow-zinc-500 shadow-lg'>
-                  <div className='flex flex-1 '>
-                    <div className='flex flex-1 justify-evenly items-center'>
-                      <div className='border-4 border-zinc-500 rounded-full w-12 h-12 items-center justify-center flex-col flex '>
+                  className="relative flex w-full min-w-[100px] flex-col justify-between bg-zinc-200 text-center text-zinc-500 shadow-lg shadow-zinc-500 last:rounded-r-md last:bg-white last:text-zinc-900"
+                >
+                  <div className="flex flex-1 ">
+                    <div className="flex flex-1 items-center justify-evenly">
+                      <div className="flex h-12 w-12 flex-col items-center justify-center rounded-full border-4 border-zinc-500 ">
                         <p>{record.reps}</p>
                         <p>rep</p>
                       </div>
-                      <div className='border-4 border-zinc-500 rounded-full w-12 h-12 items-center justify-center flex-col flex '>
+                      <div className="flex h-12 w-12 flex-col items-center justify-center rounded-full border-4 border-zinc-500 ">
                         <p>{record.sets}</p>
                         <p>set</p>
                       </div>
-                      <div className='border-4 border-zinc-500 rounded-full w-12 h-12 items-center justify-center flex-col flex '>
+                      <div className="flex h-12 w-12 flex-col items-center justify-center rounded-full border-4 border-zinc-500 ">
                         <p>{record.addedWeight}</p>
                         <p>kg</p>
                       </div>
                     </div>
-                    <div className='flex-col flex w-5 h-10'>
+                    <div className="flex h-10 w-5 flex-col">
                       <button
                         id={exercise.id}
                         value={record.date}
                         onClick={handleEditRecord}
-                        className='text-xs bg-zinc-300 hover:bg-zinc-800 text-zinc-100 w-5 h-5 rounded-tr-md'>
+                        className="h-5 w-5 rounded-tr-md bg-zinc-300 text-xs text-zinc-100 hover:bg-zinc-800"
+                      >
                         E
                       </button>
                       <hr />
@@ -86,12 +101,13 @@ export default function DisplayRecords({
                         id={exercise.id}
                         value={record.date}
                         onClick={handleDeleteRecord}
-                        className='text-xs bg-zinc-300 hover:bg-red-500 text-zinc-100 w-5 h-5 rounded-bl-md'>
+                        className="h-5 w-5 rounded-bl-md bg-zinc-300 text-xs text-zinc-100 hover:bg-red-500"
+                      >
                         x
                       </button>
                     </div>
                   </div>
-                  <p className='text-xs justify-self-end w-full rounded-br-md bg-zinc-100'>
+                  <p className="w-full justify-self-end rounded-br-md bg-zinc-100 text-xs">
                     {timestampToDate(record.date)}
                   </p>
                 </div>
@@ -100,11 +116,12 @@ export default function DisplayRecords({
           </div>
         ))}
       </div>
-      <div className='w-1/2 bg-zinc-800 h-16 flex justify-center items-center rounded-bl-md'>
+      <div className="flex h-16 w-1/2 items-center justify-center rounded-bl-md bg-zinc-800">
         <button
-          className='h-8 w-8 rounded-full bg-green-600 items-center flex justify-center'
-          onClick={handleOpenModal}>
-          <div className='text-2xl'>+</div>
+          className="flex h-8 w-8 items-center justify-center rounded-full bg-green-600"
+          onClick={handleOpenModal}
+        >
+          <div className="text-2xl">+</div>
         </button>
       </div>
     </div>
